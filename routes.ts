09a5330d1fef16f9e2e04cf73a8b6ccd2f1e5113
tuioe <qodlgks123@naver.com>
2022-07-20 14:51:49 +0900

    style:prettier apply

--- routes.ts
+++ routes.ts
@@ -1,9 +1,9 @@
 // deno-lint-ignore-file
 //Deno는 module을 URL주소를 통해서 사용가능 
-import  { Context, Router } from "https://deno.land/x/oak@v10.6.0/mod.ts"
+import { Book, TodoList } from "./types.ts";
 import { v4 } from "https://deno.land/std@0.148.0/uuid/mod.ts";
+import { Context, Router } from "https://deno.land/x/oak@v10.6.0/mod.ts"
 import { Status } from "https://deno.land/std@0.148.0/http/http_status.ts"
-import {Book,TodoList} from "./types.ts";
 
 //Router 생성 
 const router = new Router();
@@ -34,18 +34,18 @@ let todolist: TodoList[] = [
     },
 ]
 
-interface IBookRepository{
-    findBook(conid:string,context: Context): void | undefined;
+interface IBookRepository {
+    findBook(conid: string, context: Context): void | undefined;
 }
 
-class BookRepository implements IBookRepository{
-    findBook(conid:string,context:Context): void{
-        const book : Book | undefined  = books.find((b) => b.id === conid);
-        if(book){
+class BookRepository implements IBookRepository {
+    findBook(conid: string, context: Context): void {
+        const book: Book | undefined = books.find((b) => b.id === conid);
+        if (book) {
             context.response.body = book;
             context.response.status = Status.OK;// 요청성공 
         }
-        else{
+        else {
             //id가 존재 하지않을경우 에러 코드와 내용을 전달 
             context.response.body = "책을 찾지못했습니다";
             context.response.status = Status.NotFound;// 요청 리소스를 찾을수 없다 (주로 리소스가 서버에 존재하지 않을때)
@@ -53,22 +53,22 @@ class BookRepository implements IBookRepository{
     }
 }
 
-router.get('/',(context) => {
+router.get('/', (context) => {
     context.response.body = "Hello World";
 })
-    .get("/books",(context) =>{
+    .get("/books", (context) => {
         context.response.body = books;
     })
 
     //TodoList의 값을 보내줌 
-    .get("/Todos",(context) =>{
+    .get("/Todos", (context) => {
         context.response.body = todolist;
     })
-    .post("/book", async (context) =>{
+    .post("/book", async (context) => {
         const body = await context.request.body();
 
         //만약 정보를 제공하지 않았다면 
-        if(!context.request.hasBody){
+        if (!context.request.hasBody) {
 
             //400번 에러를 보내줌 
             context.response.status = Status.BadRequest //클라이언트의 잘못된 요청으로 요청에 대해 처리불가 
@@ -77,8 +77,8 @@ router.get('/',(context) => {
             context.response.body = "데이터가 없습니다"
         }
         //정보를 제공 받았다면 
-        else{
-            
+        else {
+
             //Book 객체를 선언하고 제공받은 정보(DTO)를 저장
             const book: Book = await body.value;
             //임의로 아이디를 생성
@@ -89,11 +89,11 @@ router.get('/',(context) => {
             context.response.body = book;
         }
     })
-    .get("/book/:id",async(context) =>{
-        const conid:string = context.params.id;
+    .get("/book/:id", async (context) => {
+        const conid: string = context.params.id;
         const bookrepositroy = new BookRepository();
-        bookrepositroy.findBook(conid,context);
+        bookrepositroy.findBook(conid, context);
     })
 
-    //라우터는 server에서 사용가능하도록 export로 설정 
-    export default router;
+//라우터는 server에서 사용가능하도록 export로 설정 
+export default router;
