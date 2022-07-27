e222f66a3a0e778691997779dfebe03f36a2ef57
tuioe5679 <qodlgks123@naver.com>
2022-07-27 18:27:05 +0900

    feat: TodoList Entity 생성 (DB)

---
+++ todo/src/main/java/com/GameCenter/todo/entity/TodoList.java
@@ -0,0 +1,30 @@
+package com.GameCenter.todo.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data//Getter,Setter등 기본적인 필드 메소드 생성
+@Entity//Entity 클래스로 지정하여 DB 테이블과 매핑
+@NoArgsConstructor//기본 생성자 생성
+public class TodoList {
+    @Id //PK
+    @GeneratedValue(strategy = GenerationType.AUTO) //auto increment
+    private Integer id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String date;
+
+    @Column(nullable = false)
+    private String complete;
+
+    public TodoList(String title, String date, String complete){
+        this.title = title;
+        this.date = date;
+        this.complete = complete;
+    }
+}
