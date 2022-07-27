038a38f9d97bfbf583e0251ba26535204d5cb48e
tuioe5679 <qodlgks123@naver.com>
2022-07-27 20:05:25 +0900

    feat: 조회 기능 추가

--- todo/src/main/java/com/GameCenter/todo/controller/TodoController.java
+++ todo/src/main/java/com/GameCenter/todo/controller/TodoController.java
@@ -1,15 +1,21 @@
 package com.GameCenter.todo.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import com.GameCenter.todo.entity.TodoListDTO;
+import com.GameCenter.todo.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController //객체를 반환 데이터를 JSON이나 XML으로 보냄 Controller + ResponseBody
 public class TodoController {
 
+    @Autowired
+    private TodoService todoService;
+
+    //ResponseEntity 데이터와 HTTP 상태코드를 지정하는 클래스
     @GetMapping("/")
-    @ResponseBody
-    public String Hello(){
-        return "Hello Spring";
+    public ResponseEntity list(){
+        return new ResponseEntity(todoService.TodoList(),HttpStatus.OK);
     }
 }
--- todo/src/main/java/com/GameCenter/todo/service/TodoService.java
+++ todo/src/main/java/com/GameCenter/todo/service/TodoService.java
@@ -1,12 +1,19 @@
 package com.GameCenter.todo.service;
 
+import com.GameCenter.todo.entity.TodoList;
 import com.GameCenter.todo.repositroy.TodoListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TodoService {
 
     @Autowired //타입에 해당하는 빈을 찾아 주입
     private TodoListRepository todoListRepository;
+
+    public List<TodoList> TodoList(){
+        return todoListRepository.findAll();
+    }
 }
