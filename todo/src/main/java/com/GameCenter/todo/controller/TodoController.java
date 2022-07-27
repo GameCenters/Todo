1d08afc0afdabdccba0beb8ec591c4cfc79b2160
tuioe5679 <qodlgks123@naver.com>
2022-07-27 21:24:05 +0900

    refactor: Message 객체로 값 리턴

--- todo/src/main/java/com/GameCenter/todo/controller/TodoController.java
+++ todo/src/main/java/com/GameCenter/todo/controller/TodoController.java
@@ -12,32 +12,38 @@ public class TodoController {
 
     @Autowired
     private TodoService todoService;
+    private Message message;
 
     //ResponseEntity 데이터와 HTTP 상태코드를 지정하는 클래스
     @GetMapping("/")
     public ResponseEntity list(){
-        return new ResponseEntity(todoService.TodoList(),HttpStatus.OK);
+        return new ResponseEntity(new Message(HttpStatus.OK.value(),"조회 완료",todoService.TodoList(),
+                                  todoService.TodoList().size()),HttpStatus.OK);
     }
 
     @PostMapping("/Post")
     public ResponseEntity todoAdd(@RequestBody TodoListDTO DTO){
-        return new ResponseEntity(todoService.TodoListAdd(DTO),HttpStatus.CREATED);
+        return new ResponseEntity(new Message(HttpStatus.CREATED.value(),"생성 완료",todoService.TodoListAdd(DTO),
+                                  todoService.TodoList().size()),HttpStatus.CREATED);
     }
 
     @PutMapping("/Update/{id}")
     public ResponseEntity todoUpdate(@PathVariable int id,@RequestBody TodoListDTO DTO){
-        return new ResponseEntity(todoService.TodoListUpdate(id,DTO),HttpStatus.OK);
+        return new ResponseEntity(new Message(HttpStatus.OK.value(),"수정 완료",todoService.TodoListUpdate(id,DTO),
+                                  todoService.TodoList().size()),HttpStatus.OK);
     }
 
     @DeleteMapping("/Delete/{id}")
     public ResponseEntity todoDelete(@PathVariable int id){
         todoService.TodoListDelete(id);
-        return new ResponseEntity(todoService.TodoList(),HttpStatus.OK);
+        return new ResponseEntity(new Message(HttpStatus.OK.value(),id + "번 삭제 완료",null,
+                                  todoService.TodoList().size()),HttpStatus.OK);
     }
 
     @DeleteMapping("/AllDelete")
     public ResponseEntity todoDelete(){
         todoService.TodoListAllDelete();
-        return new ResponseEntity(todoService.TodoList(),HttpStatus.OK);
+        return new ResponseEntity(new Message(HttpStatus.OK.value(),"삭제 완료",null,
+                                  todoService.TodoList().size()),HttpStatus.OK);
     }
 }
